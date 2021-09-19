d4bc028a9132676c88cfe3ec5cfca112d33f0afd
JohnnyLyimo <johnnylyimo@gmail.com>
2021-09-19 18:16:08 +0300

    Defined documentSelector div id checkout

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ var body  = document.querySelector('.right-content');
 var foods  = document.querySelector('#foods');
 var addToCardBtn  = document.querySelector('#add_to_cart');
 var resetBtn  = document.querySelector('#reset');
+var checkout  = document.querySelector('#checkout');
