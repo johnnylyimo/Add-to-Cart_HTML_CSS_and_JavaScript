62b2161ddd43ffeb8cae93b26c19d55900587fac
JohnnyLyimo <johnnylyimo@gmail.com>
2021-09-19 18:14:19 +0300

    Defined documentSelector button reset

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 var body  = document.querySelector('.right-content');
 var foods  = document.querySelector('#foods');
 var addToCardBtn  = document.querySelector('#add_to_cart');
+var resetBtn  = document.querySelector('#reset');
